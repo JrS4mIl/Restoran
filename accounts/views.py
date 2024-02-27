37be6f7fa68f38ecb9cf2f185540adc2618ab575
Yesil <yesil@gmail.com>
2024-02-27 17:02:52 +0300

    feat : Vendor model

--- accounts/views.py
+++ accounts/views.py
@@ -30,3 +30,6 @@ def registerUser(request):
         'form':form
     }
     return render(request,'accounts/registerUser.html',context)
+
+def registerVendor(request):
+    return render(request,'accounts/registerVendor.html')
