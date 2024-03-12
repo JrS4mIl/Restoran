4f63d9c68fcecc7efecf4b033f750b0d4d4c6e7d
Yesil <yesil@gmail.com>
2024-03-12 23:29:51 +0300

    feat: vendor_form

--- accounts/models.py
+++ accounts/models.py
@@ -80,6 +80,7 @@ class UserProfile(models.Model):
     address_line2=models.CharField(max_length=150,blank=True,null=True)
     country=models.CharField(max_length=15,blank=True,null=True)
     state=models.CharField(max_length=15,blank=True,null=True)
+    city=models.CharField(max_length=20,blank=True,null=True)
     pin_code=models.CharField(max_length=10,blank=True,null=True)
     latitude=models.CharField(max_length=20,blank=True,null=True)
     longitude=models.CharField(max_length=20,blank=True,null=True)
--- vendor/forms.py
+++ vendor/forms.py
@@ -2,6 +2,8 @@ from django.db import models
 from .models import Vendor
 from django import forms
 class VendorForm(forms.ModelForm):
+    vendor_license=forms.ImageField(widget=forms.FileInput(attrs={'class':'btn btn-info'}))
+
     class Meta:
         model=Vendor
         fields=['vendor_name','vendor_license']
