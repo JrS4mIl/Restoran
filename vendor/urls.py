7c7aaa73c2b05438d9b779d4b01ca9e951ce7e9e
Yesil <yesil@gmail.com>
2024-03-17 00:40:42 +0300

    feat:  Menü Oluşturucu - Kategori CRUD işlevleri

--- vendor/urls.py
+++ vendor/urls.py
@@ -4,4 +4,10 @@ from accounts import views as AccountViews
 urlpatterns = [
     path('',AccountViews.vendorDashboard,name='vendor'),
     path('profile/',vprofile,name='vprofile'),
+    path('menu-builder',menu_builder,name='menu_builder'),
+    path('menu-builder/category/<int:pk>/',fooditems_by_category,name='fooditems_by_category'),
+    path('menu-builder/category/add/', add_category, name='add_category'),
+    path('menu-builder/category/edit/<int:pk>/', edit_category, name='edit_category'),
+    path('menu-builder/category/delete/<int:pk>/', delete_category, name='delete_category')
+
 ]
