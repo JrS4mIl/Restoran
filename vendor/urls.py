60f6953576ebea6adadabd5a4f6f6dad9aaf449f
Yesil <yesil@gmail.com>
2024-03-18 20:29:23 +0300

    feat: add edit delete food dashboard

--- vendor/urls.py
+++ vendor/urls.py
@@ -8,6 +8,11 @@ urlpatterns = [
     path('menu-builder/category/<int:pk>/',fooditems_by_category,name='fooditems_by_category'),
     path('menu-builder/category/add/', add_category, name='add_category'),
     path('menu-builder/category/edit/<int:pk>/', edit_category, name='edit_category'),
-    path('menu-builder/category/delete/<int:pk>/', delete_category, name='delete_category')
+    path('menu-builder/category/delete/<int:pk>/', delete_category, name='delete_category'),
+
+    #FoodItemCrud
+    path('menu-builder/food/add',add_food,name='add_food'),
+    path('menu-builder/food/edit/<int:pk>/', edit_food, name='edit_food'),
+    path('menu-builder/food/delete/<int:pk>/', delete_food, name='delete_food'),
 
 ]
