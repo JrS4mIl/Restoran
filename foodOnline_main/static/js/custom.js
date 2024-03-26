f3117b2e86e73d8e2ae9e0a3a0dda3dd9046d990
Yesil <yesil@gmail.com>
2024-03-26 16:30:21 +0300

    feat: : Ön uçla sepet işlevleri

--- foodOnline_main/static/js/custom.js
+++ foodOnline_main/static/js/custom.js
@@ -82,6 +82,12 @@ $(document).ready(function(){
                 else{
                     $('#cart_counter').html(response.cart_counter['cart_count'])
                     $('#qty-'+food_id).html(response.qty)
+                    //subtotal tex and grand
+                    applyCartAmounts(
+                        response.cart_amount['subtotal'],
+                        response.cart_amount['tax'],
+                        response.cart_amount['grand_total']
+                    )
 
                 }
             }
@@ -98,6 +104,7 @@ $(document).ready(function(){
 
         food_id=$(this).attr('data-id');
         url=$(this).attr('data-url');
+        cart_id=$(this).attr('id');
 
         $.ajax({
             type:'GET',
@@ -114,6 +121,17 @@ $(document).ready(function(){
                 }else{
                     $('#cart_counter').html(response.cart_counter['cart_count'])
                     $('#qty-'+food_id).html(response.qty)
+                    applyCartAmounts(
+                        response.cart_amount['subtotal'],
+                        response.cart_amount['tax'],
+                        response.cart_amount['grand_total']
+                    )
+
+                    if(window.location.pathname=='/cart/'){
+                        removeCartItem(response.qty,cart_id)
+                        checkEmptyCart()
+
+                    }
 
                 }
 
@@ -138,8 +156,13 @@ $(document).ready(function(){
                 }else{
                     $('#cart_counter').html(response.cart_counter['cart_count'])
                     swal(response.status,response.message,'success')
+                    applyCartAmounts(
+                        response.cart_amount['subtotal'],
+                        response.cart_amount['tax'],
+                        response.cart_amount['grand_total']
+                    )
                     removeCartItem(0,cart_id)
-                    checkEmptyCart()
+
 
                 }
 
@@ -149,11 +172,11 @@ $(document).ready(function(){
 
 
     //delete the cart element if the qty is 0
-    function removeCartItem(cartItemQty,cart_id){
+function removeCartItem(cartItemQty,cart_id){
         if(cartItemQty<=0){
             document.getElementById('cart-item-'+cart_id).remove()
         }
-    }
+}
 
     //check
     function checkEmptyCart(){
@@ -161,6 +184,17 @@ $(document).ready(function(){
         if(cart_counter==0){
             document.getElementById('empty-cart').style.display='block'
         }
+    }
+
+    function applyCartAmounts(subtotal,tax,grand_total){
+        if(window.location.pathname=='/cart/'){
+            $('#subtotal').html(subtotal)
+            $('#tax').html(tax)
+            $('#total').html(grand_total)
+        }
+
+
+
     }
 
 
--- marketplace/views.py
+++ marketplace/views.py
@@ -5,7 +5,7 @@ from vendor.models import Vendor
 from menu.models import Category,FoodItem
 from django.db.models import Prefetch
 
-from .context_processors import get_cart_counter
+from .context_processors import get_cart_counter,get_cart_amounts
 from .models import Cart
 from django.contrib.auth.decorators import login_required
 # Create your views here.
@@ -48,10 +48,10 @@ def add_to_cart(request, food_id=None):
                     chkCart = Cart.objects.get(user=request.user, fooditem=fooditem)
                     chkCart.quantity += 1
                     chkCart.save()
-                    return JsonResponse({'status': 'success', 'message': 'Increased the cart quantity','cart_counter':get_cart_counter(request),'qty':chkCart.quantity})
+                    return JsonResponse({'status': 'success', 'message': 'Increased the cart quantity','cart_counter':get_cart_counter(request),'qty':chkCart.quantity,'cart_amount':get_cart_amounts(request)})
                 except Cart.DoesNotExist:
                     chkCart = Cart.objects.create(user=request.user, fooditem=fooditem, quantity=1)
-                    return JsonResponse({'status': 'success', 'message': 'Added the food to the cart','cart_counter':get_cart_counter(request),'qty':chkCart.quantity})
+                    return JsonResponse({'status': 'success', 'message': 'Added the food to the cart','cart_counter':get_cart_counter(request),'qty':chkCart.quantity,'cart_amount':get_cart_amounts(request)})
 
             except FoodItem.DoesNotExist:
                 return JsonResponse({'status': 'Failed', 'message': 'This food does not exist!'})
@@ -75,7 +75,7 @@ def decrease_cart(request, food_id=None):
                     else:
                         chkCart.delete()
                         chkCart.quantity=0
-                    return JsonResponse({'status': 'success', 'message': 'Increased the cart quantity','cart_counter':get_cart_counter(request),'qty':chkCart.quantity})
+                    return JsonResponse({'status': 'success', 'message': 'Increased the cart quantity','cart_counter':get_cart_counter(request),'qty':chkCart.quantity,'cart_amount':get_cart_amounts(request)})
                 except Cart.DoesNotExist:
                     return JsonResponse({'status': 'Failed', 'message': 'You do not have this item in your cart'})
 
@@ -88,7 +88,7 @@ def decrease_cart(request, food_id=None):
 
 @login_required(login_url='login')
 def cart(request):
-    cart_items=Cart.objects.filter(user=request.user)
+    cart_items=Cart.objects.filter(user=request.user).order_by('created_at')
     context={
         'cart_items':cart_items,
     }
@@ -101,7 +101,7 @@ def delete_cart(request,cart_id):
                 cart_item=Cart.objects.get(user=request.user,id=cart_id)
                 if cart_item:
                     cart_item.delete()
-                    return JsonResponse({'status': 'Success', 'message': 'Cart item has been deleted!.','cart_counter':get_cart_counter(request)})
+                    return JsonResponse({'status': 'Success', 'message': 'Cart item has been deleted!.','cart_counter':get_cart_counter(request),'cart_amount':get_cart_amounts(request)})
             except:
                 return JsonResponse({'status': 'Failed', 'message': 'Cart item does not exist!'})
         else:
