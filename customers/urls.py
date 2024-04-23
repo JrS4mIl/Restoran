81b160e0728bdaea58d4438e223ce98135926be2
Yesil <yesil@gmail.com>
2024-04-23 18:29:30 +0300

    feat: : Customer Dashboard

--- customers/urls.py
+++ customers/urls.py
@@ -3,6 +3,8 @@ from .views import *
 from accounts import views as AccountViews
 urlpatterns=[
     path('',AccountViews.custDashboard,name='customer'),
-    path('profile/',cprofile,name='cprofile')
+    path('profile/',cprofile,name='cprofile'),
+    path('my-orders/',my_orders,name='customer_my_orders'),
+    path('order-detail/<int:order_number>/',order_detail,name='order_detail')
 
 ]
